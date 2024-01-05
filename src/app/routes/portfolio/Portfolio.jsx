485870634523f4755084577295f9e1b07e82cb41
Tomas Serra <tomi.serra@gmail.com>
2024-01-05 12:49:55 -0300

    Cambio de titulo

--- src/app/routes/portfolio/Portfolio.jsx
+++ src/app/routes/portfolio/Portfolio.jsx
@@ -2,34 +2,12 @@ import React from 'react'
 import ProductCard from '../../../components/ProductCard/ProductCard';
 import './Portfolio.scss'
 
-import { AiFillDatabase } from 'react-icons/ai';
-import { BsFillPencilFill, BsFillPeopleFill, BsCurrencyDollar } from 'react-icons/bs';
-
 function Portfolio() {
   return (
-    <>
-        <section className='events-portfolio'>
-            <div className='events-header'>
-            <span>
-                <BsFillPencilFill />
-                100% personalizables
-            </span>
-            <span>
-                <BsFillPeopleFill />
-                Eventos corporativos
-            </span>
-            <span>
-                <BsCurrencyDollar />
-                Cotización en el momento
-            </span>
-            <span>
-                <AiFillDatabase />
-                Base de datos
-            </span>
-            </div>
-            <div className='container'>
-                <p>Recordá que todos los juegos y aplicaciones de nuestro catálogo son 100% personalizables. De requerir un juego o aplicación que no este listado en el catálogo, se puede cotizar sin problema mandandonos un mail o mismo desde el botón de “COTIZAR”.</p>
-            </div>
+    <div className='portfolio-container'>
+        <section className='title'>
+            <h1>Nuestro portfolio de plantillas</h1>
+            <p>En nuestro portfolio podras ver aplicaciones y juegos ya hechos por nosotros, los cuales pueden ser utilizados como plantilla ademas de contar con la posibilidad de personalizarlos a tu gusto y hasta combinarlos.</p>
         </section>
 
         <section className='bestselling-portfolio'>
@@ -76,8 +54,8 @@ function Portfolio() {
                     <img src='./assets/products/wordsearch.png' alt='Wordsearch' />
                 </ProductCard>
             </div>
-      </section>
-    </>
+        </section>
+    </div>
   )
 }
 
